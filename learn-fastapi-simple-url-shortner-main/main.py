eeebf9f71839c6a13db51e6a9a03e39477fdace7
saraswat-garvit <75795109+saraswat-garvit@users.noreply.github.com>
2023-06-23 21:43:28 +0530

    Delete learn-fastapi-simple-url-shortner-main directory

--- learn-fastapi-simple-url-shortner-main/main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from url_shortener import router
-
-# Create the app
-app = FastAPI()
-
-# Add the router
-app.include_router(router.url_shortener, prefix="/url-shortener")
